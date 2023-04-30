22348ba0c7f9a8ac0fa6fde806d83a6af4c40453
judyab <72942094+judyab@users.noreply.github.com>
2023-04-30 22:04:41 +0200

    Delete config directory

--- config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-    .connect("mongodb+srv://judyabdallah1:birdy0705@birdy.nsthjak.mongodb.net/birdy")
-    // .connect("mongodb+srv://"+process.env.DB_USER_PASS +"@birdy.nsthjak.mongodb.net/birdy")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log("Failed to connect to MongoDB", err));
